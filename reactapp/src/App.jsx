e2da9b06500a02c202b3d4e6e963cf6f1eb2b971
KARIN <KARIN@KARIN4696-FLOW>
2023-09-21 17:39:34 +0900

    プロジェクト ファイルを追加します。

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateWeatherData();
+    }
+
+    static renderForecastsTable(forecasts) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp. (C)</th>
+                        <th>Temp. (F)</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.date}>
+                            <td>{forecast.date}</td>
+                            <td>{forecast.temperatureC}</td>
+                            <td>{forecast.temperatureF}</td>
+                            <td>{forecast.summary}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+            : App.renderForecastsTable(this.state.forecasts);
+  //      const { todos } = this.state;
+
+        //return (<div>
+        //    <input type="text" onInput={this.onInput} />
+        //    <button onClick={this.addTodo} > ìoò^</button>
+
+        //    <ul>
+        //        {todos.map((todo, index) => <li keu={index}>
+        //            {todo}
+        //            <button onClick={() => { this.removeTodo(index) }}>çÌèú</button>
+        //            <button onClick={this.addFin}> äÆóπ</button>
+        //        </li>)}
+        //    </ul>
+        //</div>);
+        return (
+            <div>
+                <h1 id="tabelLabel" >Weather forecast</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        this.setState({ forecasts: data, loading: false });
+    }
+}
